3c5b84abb73bdd109b32a0364b7ab216fc41c1e3
Yan Paulo <yanpaulo@hotmail.com>
2018-04-13 03:18:07 -0300

    Stuff.

--- UserApp/UserApp/Views/StartPage.xaml.cs
+++ UserApp/UserApp/Views/StartPage.xaml.cs
@@ -16,12 +16,15 @@ namespace UserApp.Views
     {
         private bool isScanning;
         private MobileBarcodeScanner scanner;
+        private NotificationHubClient hub;
+
         public StartPage()
         {
             InitializeComponent();
             scanner = new MobileBarcodeScanner();
+            hub = NotificationHubClient.CreateClientFromConnectionString("Endpoint=sb://yanscorp.servicebus.windows.net/;SharedAccessKeyName=DefaultFullSharedAccessSignature;SharedAccessKey=MVZlt7280W+ncv+ad1QoA8Y5GG5bxQpB/8zTPe9YnBo=", "MCastNotificationHub");
         }
-        
+
 
         private async void Button_Clicked(object sender, EventArgs args)
         {
@@ -30,15 +33,6 @@ namespace UserApp.Views
             await RegisterAsync(id);
         }
 
-        private async Task RegisterAsync(string id)
-        {
-            var hub = NotificationHubClient.CreateClientFromConnectionString("Endpoint=sb://yanscorp.servicebus.windows.net/;SharedAccessKeyName=DefaultListenSharedAccessSignature;SharedAccessKey=Nt/JE/8Zc/F1ZwQ1mO/ZcM/NUg0CRs3DjI31IK7hTNM=", "MCastNotificationHub");
-            var registration = await DependencyService.Get<INotificationService>().GetRegistrationDescriptionAsync(new[] { $"group:{id}" });
-            registration.RegistrationId = await hub.CreateRegistrationIdAsync();
-            //registration.id
-            var description = await hub.CreateOrUpdateRegistrationAsync(registration);
-            await DisplayAlert("Aviso", "Registrado", "Ok");
-        }
 
         private async void QR_Clicked(object sender, EventArgs args)
         {
@@ -46,12 +40,36 @@ namespace UserApp.Views
             {
                 isScanning = true;
                 var code = await scanner.Scan();
-                await RegisterAsync(code.Text);
+                if (code != null)
+                {
+                    await RegisterAsync(code.Text); 
+                }
             }
             finally
             {
                 isScanning = false;
             }
+        }
+
+
+        private async void CleanButton_Clicked(object sender, EventArgs args)
+        {
+            var registrations = await hub.GetAllRegistrationsAsync(100);
+            foreach (var registration in registrations)
+            {
+                await hub.DeleteRegistrationAsync(registration);
+            }
+            await DisplayAlert("Aviso", "Removido", "Ok");
+        }
+
+
+        private async Task RegisterAsync(string id)
+        {
+            var registration = await DependencyService.Get<INotificationService>().GetRegistrationDescriptionAsync(new[] { $"group:{id}" });
+            registration.RegistrationId = await hub.CreateRegistrationIdAsync();
+            //registration.id
+            var description = await hub.CreateOrUpdateRegistrationAsync(registration);
+            await DisplayAlert("Aviso", "Registrado", "Ok");
         }
 
         protected override bool OnBackButtonPressed()
